45188d9f39e09bfabc7c55985742b9d88dd050b7
Kalyan Rohan <s3958905@student.rmit.edu.au>
2022-10-06 20:54:16 +1100

    Update App.java

--- Perfect Event Agency/src/App.java
+++ Perfect Event Agency/src/App.java
@@ -14,19 +14,19 @@ public class App {
         int choice = 0;
         //test data
         ArrayList<Venue> venues = new ArrayList<Venue>();
-        venues.add(new Venue("The Ballroom", 100, 15000, true));
-        venues.add(new Venue("The Grand Hall", 50, 5000, true));
-        venues.add(new Venue("The Grand Ballroom", 120, 8000, true));
-        venues.add(new Venue("The Westin", 200, 20000, true));
-        venues.add(new Venue("The Hilton", 30, 2000, true));
-        venues.add(new Venue("The Marriott", 150, 10000, true));
-        venues.add(new Venue("The Sheraton", 15, 1000, true));
+        venues.add(new Venue("The Ballroom", 100, 15000, true,1));
+        venues.add(new Venue("The Grand Hall", 50, 5000, true,2));
+        venues.add(new Venue("The Grand Ballroom", 120, 8000, true,3));
+        venues.add(new Venue("The Westin", 200, 20000, true,4));
+        venues.add(new Venue("The Hilton", 30, 2000, true,5));
+        venues.add(new Venue("The Marriott", 150, 10000, true,6));
+        venues.add(new Venue("The Sheraton", 15, 1000, true,7));
 
         ArrayList<FoodSelection> foodSelections = new ArrayList<FoodSelection>();
-        foodSelections.add(new FoodSelection("Food Plan A", 1000));
-        foodSelections.add(new FoodSelection("Food Plan B", 2000));
-        foodSelections.add(new FoodSelection("Food Plan C", 3000));
-        foodSelections.add(new FoodSelection("Food Plan D", 4000));
+        foodSelections.add(new FoodSelection("Food Plan A", 1000,1));
+        foodSelections.add(new FoodSelection("Food Plan B", 2000, 2));
+        foodSelections.add(new FoodSelection("Food Plan C", 3000, 3));
+        foodSelections.add(new FoodSelection("Food Plan D", 4000, 4));
 
         ArrayList<Packages> packages = new ArrayList<Packages>();
         packages.add(new Packages(1, "Package A FROM $1100", 15));
@@ -433,97 +433,90 @@ public class App {
                         case 1:
                             //View Packages
                             //ENTER CODE HERE
-                            app.printSeperator();
-                            for (int i = 0; i < packages.size(); i++) {
-                                packages.get(i).str();
-                            }
+                            customer.viewPackages(packages);
                             break;
                         case 2:
-                            //Book Event
+                            //Create Booking
                             //ENTER CODE HERE
-                            Booking booking = new Booking();
+
+                            Booking booking =new Booking();
+
                             //selecting package
-                            app.printSeperator();
-                            for (int i = 0; i < packages.size(); i++) {
-                                packages.get(i).str();
-                            }
-                            System.out.println("Enter the package number you want to book");
-                            booking.selectPackage(packages.get(sc1.nextInt() - 1));
+                            customer.viewPackages(packages);
+                            System.out.println("Enter the package id number you want to book");
+                            int packageId=sc1.nextInt();
+                            booking.selectPackage(packages,packageId);
 
-                            app.printSeperator();
+                            //entering the number of attendees
+                            System.out.println("Enter the number of attendees: ");
+                            booking.setAttendees(sc1.nextInt());
+
                             //selecting venue
-                            System.out.println("Enter the number of guests: ");
-                            int numberOfGuests = sc1.nextInt();
-                            for (int i = 0; i < venues.size(); i++) {
-                                if (venues.get(i).getCapacity() >= numberOfGuests && venues.get(i).isAvailability() == true) {
-                                    System.out.println(i + 1);
-                                    customer.viewVenue(venues.get(i));
-                                }
-                            }
-                            System.out.println("Enter the venue number you want to book");
-                            booking.selectVenue(venues.get(sc1.nextInt() - 1));
+                            customer.showAvailableVenues(venues, booking);
+                            System.out.println("Enter the venue id number you want to book: ");
+                            int venueId=sc1.nextInt();
+                            booking.selectVenue(venues,venueId);
 
-                            app.printSeperator();
-                            //enter food selection
-                            for (int i = 0; i < foodSelections.size(); i++) {
-                                System.out.println(i + 1);
-                                customer.viewFood(foodSelections.get(i));
-                            }
-                            System.out.println("Enter the food number you want to book");
-                            booking.foodSelection(foodSelections.get(sc1.nextInt() - 1));
+                            //selecting food and beverage
+                            customer.viewFood(foodSelections);
+                            System.out.println("Enter the food id number you want to book: ");
+                            int foodId=sc1.nextInt();
+                            booking.SelectFood(foodSelections, foodId);
 
-                            app.printSeperator();
-                            //selecting options
+                            //select options
                             Options option = new Options();
-                            System.out.println("Do you want to hire a band? (Y/N)");
-                            if (sc.next().toUpperCase().equals("Y")) {
-                                option.setIsHireBand(true);
-                            }
-
-                            System.out.println("Do you want to have flower decorations? (Y/N)");
-                            if (sc.next().toUpperCase().equals("Y")) {
-                                option.setIsFlowerDecoration(true);
-                            }
-
-                            System.out.println("Do you want to have sound system? (Y/N)");
-                            if (sc.next().toUpperCase().equals("Y")) {
+                            System.out.println("Do you want a soundsystem? (y/n)");
+                            String soundSystem=sc1.next();
+                            if(soundSystem.equals("y")){
                                 option.setIsSoundSystem(true);
                             }
-
-                            app.printSeperator();
-
-                            booking.selectOptions(option);
-                            //to ensure that the code doesnt skip the input
-                            sc.nextLine();
-                            //entering customer details
-                            System.out.println("Enter your first name: ");
-                            customer.setFirstName(sc.nextLine());
-                            System.out.println("Enter your last name: ");
-                            customer.setLastName(sc.nextLine());
-                            System.out.println("Enter your dob: ");
-                            customer.setDob(sc.nextLine());
-                            System.out.println("Enter your address: ");
-                            customer.setAddress(sc.nextLine());
-
-                            //entering payment details
-                            System.out.println("Select your payment method: \n1. Credit card\n 2.Debit card\n 3. Net banking (Enter String)");
-                            int payment_choice = sc.nextInt();
-                            String paymentMethod = "";
-                            if (payment_choice == 1) {
-                                paymentMethod = "Credit card";
-                            } else if (payment_choice == 2) {
-                                paymentMethod = "Debit card";
-                            } else if (payment_choice == 3) {
-                                paymentMethod = "Net banking";
+                            System.out.println("Do you want a musical band? (y/n)");
+                            String musicalBand=sc1.next();
+                            if(musicalBand.equals("y")){
+                                option.setIsHireBand(true);
                             }
-                            app.printSeperator();
-                            customer.createBooking(customer, booking, paymentMethod);
+                            System.out.println("Do you want flower decorations? (y/n)");
+                            String flowers=sc1.next();
+                            if(flowers.equals("y")){
+                                option.setIsFlowerDecoration(true);
+                            }
+                            booking.selectOptions(option);
+                            
+                            //create booking
+                            System.out.println("Enter your first name: ");
+                            String firstName=sc1.next();
+                            System.out.println("Enter your last name: ");
+                            String lastName=sc1.next();
+                            System.out.println("Enter your date of birth: ");
+                            String dob=sc1.next();
+                            System.out.println("Enter your address: ");
+                            String address=sc1.next();
+                            System.out.println("Enter your phone number: ");
+                            int phone=sc1.nextInt();
+                            System.out.println("Select a payment method (enter number): \n1. Credit Card \n2. Debit Card \n3. Net Banking");
+                            int paymentMethod=sc1.nextInt();
+                            switch (paymentMethod) {
+                                case 1:
+                                    customer.createBooking(customer,booking, "Credit Card", 
+                                    firstName, lastName, dob, address, phone);
+                                    break;
+                                case 2:
+                                customer.createBooking(customer,booking, "Debit Card", 
+                                    firstName, lastName, dob, address, phone);
+                                    break;
+
+                                case 3:
+                                customer.createBooking(customer,booking, "Net Banking", 
+                                    firstName, lastName, dob, address, phone);
+                                    break;
+                            }
+
                             break;
                         case 3:
                             //Track Progress
                             //ENTER CODE HERE
                             app.printSeperator();
-                            if (customer.getEvents().size() == 0) {
+                            if (customer.getEvents().size()==0){
                                 System.out.println("You have not booked any events yet");
                             } else {
                                 customer.viewEvents();
@@ -543,41 +536,31 @@ public class App {
                             //Make Complaint
                             //ENTER CODE HERE
                             app.printSeperator();
-                            if (customer.getEvents().size() != 0) {
-                                customer.viewEvents();
-                                System.out.println("Enter Event ID: ");
-                                int eventID = sc1.nextInt();
-                                for (int i = 0; i < customer.getEvents().size(); i++) {
-                                    if (customer.getEvents().get(i).getEventID() == eventID) {
-                                        System.out.println("Enter your complaint: ");
-                                        sc.nextLine();
-                                        customer.makeComplaint(customer.getEvents().get(i), sc1.nextLine());
-                                    }
-                                }
-
-                            } else {
-                                System.out.println("You have not booked any events yet");
+                            if (customer.viewEvents()==true){
+                                System.out.println("Enter the event number you want to make a complaint for: ");
+                                int eventNumber = sc1.nextInt();
+                                System.out.println("Enter your complaint: ");
+                                customer.makeComplaint(eventNumber, sc1.nextLine());
                             }
                             break;
                         case 6:
                             //Request Refund and Cancel
                             //ENTER CODE HERE
                             app.printSeperator();
-                            if (customer.getEvents().size() != 0) {
-                                customer.viewEvents();
-                                System.out.println("Enter Event ID: ");
-                                int eventID1 = sc1.nextInt();
-                                sc.nextLine();
-                                String details = sc1.nextLine();
-                                for (int i = 0; i < customer.getEvents().size(); i++) {
-                                    if (customer.getEvents().get(i).getEventID() == eventID1) {
-                                        customer.requestRefund(customer, customer.getEvents().get(i), details);
+                            if (customer.viewEvents()==true){
+                                System.out.println("Enter the event number you want to cancel: ");
+                                int eventNumber = sc1.nextInt();
+                                System.out.println("Enter the details to your refund: ");
+                                String details= sc.nextLine();
+                                for (EventID event: customer.getEvents()){
+                                    if (event.getEventID()==eventNumber){
+                                        customer.requestRefund(customer, event,details);
                                     }
-                                }
-                            } else {
-                                System.out.println("You have no events to cancel");
-                            }
 
+                                }
+                                
+                            }
+                            
                             break;
                         case 7:
                             //Change Booking
@@ -588,15 +571,15 @@ public class App {
                             //check balance
                             //ENTER CODE HERE
                             app.printSeperator();
-                            System.out.println("Your balance is: " + customer.getBalance());
+                            System.out.println("Your balance is: "+customer.getBalance());
                             break;
                         case 9:
                             //Deposit Money
                             //ENTER CODE HERE
                             app.printSeperator();
                             System.out.println("Enter the amount you want to deposit: ");
-                            customer.setBalance(sc.nextFloat() + customer.getBalance());
-                            System.out.println("Your new balance is: " + customer.getBalance());
+                            customer.setBalance(sc.nextFloat()+customer.getBalance());
+                            System.out.println("Your new balance is: "+customer.getBalance());
                             break;
                         case 10:
                             //view customer details
@@ -604,6 +587,14 @@ public class App {
                             app.printSeperator();
                             customer.customerDetails();
                             break;
+                        case 11:
+                            //Book Inspection
+                            customer.viewAllVenues(venues);
+                            System.out.println("Enter the venue id number you want to book an inspection for: ");
+                            int venueId2=sc1.nextInt();
+                            customer.bookInspection(venues, venueId2);
+
+                            
                         default:
                             System.out.println("Invalid choice");
                             break;
